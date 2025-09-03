8dfde1094eb487b0adecbc53c88f25e5b98336aa
Chandrahas Kasoju <kasoju.chandrahas@gmail.com>
2025-09-03 12:40:04 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,4 @@ export ROS_DISTRO=jazzy
 export ROS_DOMAIN_ID=0
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 export TAPPAS_POST_PROC_DIR=$(pkg-config --variable=tappas_postproc_lib_dir hailo-tappas-core)
-/usr/bin/supervisord
 exec "$@"
