ddd72f596b08a5e37f62ccf8a591ff65abf35352
Chandrahas Kasoju <kasoju.chandrahas@gmail.com>
2025-10-23 12:05:53 +0200

    added time _stamp to camera_info

--- ros2_ws/src/sensors_bringup/sensors_bringup/camera_bringup.py
+++ ros2_ws/src/sensors_bringup/sensors_bringup/camera_bringup.py
@@ -61,6 +61,7 @@ class Camera(Node):
         top_flipped = cv2.flip(top_flipped, 1)
         ros_image = self.bridge.cv2_to_imgmsg(top_flipped, encoding="bgr8")
         ros_image.header.stamp = self.get_clock().now().to_msg()
+        self.msg.header.stamp = ros_image.header.stamp
         ros_image.header.frame_id = "thermal_camera_link"
         self.det_pub.publish(ros_image)
         self.pub.publish(self.msg)
