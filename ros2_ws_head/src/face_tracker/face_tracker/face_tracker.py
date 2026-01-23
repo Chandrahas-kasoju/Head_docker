5fa6a20d29f5d292f712f367f780c56fbcb973d9
Chandrahas Kasoju <kasoju.chandrahas@gmail.com>
2026-01-23 16:26:09 +0100

    edit tracker intergers

--- ros2_ws_head/src/face_tracker/face_tracker/face_tracker.py
+++ ros2_ws_head/src/face_tracker/face_tracker/face_tracker.py
@@ -22,6 +22,10 @@ class FaceTrackerNode(Node):
         pitch_topic = self.get_parameter('pitch_topic').get_parameter_value().string_value
         roll_topic = self.get_parameter('roll_topic').get_parameter_value().string_value
         eye_center_topic = self.get_parameter('eye_center_topic').get_parameter_value().string_value
+        self.pitch_cmd = Int32()
+        self.roll_cmd = Int32()
+        self.pitch_cmd.data = 2
+        self.roll_cmd.data = 2
         
         self.subscription = self.create_subscription(
             Point2D,
@@ -35,7 +39,7 @@ class FaceTrackerNode(Node):
         self.get_logger().info('Face tracker CONTROL node has been started. Waiting for eye center data...')
 
         # --- Intent Subscription ---
-        self.current_intent = "UNKNOWN"
+        self.current_intent = "NO_PERSON_DETECTED"
         self.intent_subscription = self.create_subscription(
             String,
             '/person_intent',
@@ -46,8 +50,16 @@ class FaceTrackerNode(Node):
         self.current_intent = msg.data
 
     def eye_center_callback(self, msg):
+
+        
+
         # Only track if the person wants to interact
-        if self.current_intent != "CLOSE_PROXIMITY":
+
+        if self.current_intent != "CLOSE_PROXIMITY"  or self.current_intent != "WANT_TO_INTERACT":
+            self.roll_cmd.data = 2  # Default: No movement
+            self.pitch_cmd.data = 2  # Default: No movement
+            self.roll_publisher.publish(self.roll_cmd)
+            self.pitch_publisher.publish(self.pitch_cmd)
             return
 
         width = self.get_parameter('image_width').get_parameter_value().integer_value
@@ -55,13 +67,13 @@ class FaceTrackerNode(Node):
         
         center_x = width // 2
         center_y = height // 2
+        self.roll_cmd.data = 0
+        self.pitch_cmd.data = 0
 
-        pitch_cmd = Int32()
-        roll_cmd = Int32()
+        
         
         # Default: No movement
-        pitch_cmd.data = 0
-        roll_cmd.data = 0
+        
         
         dead_zone_percent = self.get_parameter('dead_zone_percent').get_parameter_value().integer_value
         dead_zone_x = (width * dead_zone_percent) // 200 # Divided by 200 because percent is split on both sides
@@ -73,18 +85,19 @@ class FaceTrackerNode(Node):
         # --- Pitch and Roll Calculation ---
         # Roll (left/right)
         if target_x < center_x - dead_zone_x: 
-            roll_cmd.data = 1 # Move left 
+            self.roll_cmd.data = 1 # Move left 
         elif target_x > center_x + dead_zone_x:
-            roll_cmd.data = -1 # Move right 
+            self.roll_cmd.data = -1 # Move right 
         
         # Pitch (up/down)
         if target_y < center_y - dead_zone_y:
-            pitch_cmd.data = 1 
+            self.pitch_cmd.data = 1 
         elif target_y > center_y + dead_zone_y:
-            pitch_cmd.data = -1 
+            self.pitch_cmd.data = -1 
 
-        self.pitch_publisher.publish(pitch_cmd)
-        self.roll_publisher.publish(roll_cmd)
+        # --- Publish Commands ---
+        self.roll_publisher.publish(self.roll_cmd)
+        self.pitch_publisher.publish(self.pitch_cmd)
 
 def main(args=None):
     rclpy.init(args=args)
